3aece80503e801e2e1683c14183463d307e15147
Khaled Sharaf Eldin <wi15b189@technikum-wien.at>
2018-11-17 18:27:54 +0100

    Initial commit

---
+++ app/src/main/java/technikum_wien/at/smartcity_indoornavigation/MainActivity.java
@@ -0,0 +1,13 @@
+package technikum_wien.at.smartcity_indoornavigation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
