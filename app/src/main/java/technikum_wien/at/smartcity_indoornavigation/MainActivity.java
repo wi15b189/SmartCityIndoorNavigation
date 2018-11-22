ca603e04d5386091d44e213ce0b433238245a075
Khaled <wi15b189@technikum-wien.at>
2018-11-22 22:39:55 +0100

    added img view (Temp plan) and some sort of zoom functionality
    (cherry picked from commit 8de81f47eae6ee66589ab705aee411ea2aad85e9)
    
    # Conflicts:
    #	.idea/vcs.xml
    #	app/src/main/java/technikum_wien/at/smartcity_indoornavigation/MainActivity.java
    #	app/src/main/res/layout/activity_main.xml

--- app/src/main/java/technikum_wien/at/smartcity_indoornavigation/MainActivity.java
+++ app/src/main/java/technikum_wien/at/smartcity_indoornavigation/MainActivity.java
@@ -1,13 +1,41 @@
 package technikum_wien.at.smartcity_indoornavigation;
 
-import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.Activity;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
+
+    private ScaleGestureDetector mScaleGestureDetector;
+    private float mScaleFactor = 1.0f;
+    private ImageView mImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mImageView=findViewById(R.id.imageView);
+        mScaleGestureDetector = new ScaleGestureDetector(this, new ScaleListener());
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent motionEvent) {
+        mScaleGestureDetector.onTouchEvent(motionEvent);
+        return true;
+    }
+
+    private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
+        @Override
+        public boolean onScale(ScaleGestureDetector scaleGestureDetector){
+            mScaleFactor *= scaleGestureDetector.getScaleFactor();
+            mScaleFactor = Math.max(0.1f,
+                    Math.min(mScaleFactor, 10.0f));
+            mImageView.setScaleX(mScaleFactor);
+            mImageView.setScaleY(mScaleFactor);
+            return true;
+        }
     }
 }
---
+++ app/src/main/java/technikum_wien/at/smartcity_indoornavigation/MyView.java
@@ -0,0 +1,4 @@
+package technikum_wien.at.smartcity_indoornavigation;
+
+public class MyView {
+}
