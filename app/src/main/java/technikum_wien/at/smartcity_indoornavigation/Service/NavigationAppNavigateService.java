0965af9a5dc6fc82eb95628ebf85b617ef01a9fc
Khaled <wi15b189@technikum-wien.at>
2018-12-12 13:58:02 +0100

    adapted methods in services todo fully implement service

--- app/src/main/java/technikum_wien/at/smartcity_indoornavigation/Service/NavigationAppNavigateService.java
+++ app/src/main/java/technikum_wien/at/smartcity_indoornavigation/Service/NavigationAppNavigateService.java
@@ -1,18 +1,20 @@
 package technikum_wien.at.smartcity_indoornavigation.Service;
 
+import java.util.HashSet;
 import java.util.List;
 
 import technikum_wien.at.smartcity_indoornavigation.Entities.AccessPoint;
 import technikum_wien.at.smartcity_indoornavigation.Entities.Stand;
 
 public class NavigationAppNavigateService {
-//    public getAPs (){
-//        List<AccessPoint> temp = new List<AccessPoint>();
-//        return temp;
-//    }
 
-//   public getStands (){
-//     List<Stand> temp = new List<Stand>();
-//        return temp;
-//    }
+   public HashSet getAPs (){
+       HashSet<AccessPoint> temp = new HashSet<AccessPoint>();
+       return temp;
+   }
+
+  public HashSet<Stand> getStands (){
+    HashSet<Stand> temp = new HashSet<Stand>();
+       return temp;
+   }
 }
