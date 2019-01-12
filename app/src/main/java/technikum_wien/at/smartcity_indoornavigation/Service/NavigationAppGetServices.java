c34bb02a4b5736e424818c12144fe36f6b6828a7
Khaled <wi15b189@technikum-wien.at>
2019-01-12 13:26:50 +0100

    refacturing

---
+++ app/src/main/java/technikum_wien/at/smartcity_indoornavigation/Service/NavigationAppGetServices.java
@@ -0,0 +1,20 @@
+package technikum_wien.at.smartcity_indoornavigation.Service;
+
+import java.util.HashSet;
+import java.util.List;
+
+import technikum_wien.at.smartcity_indoornavigation.Entities.AccessPoint;
+import technikum_wien.at.smartcity_indoornavigation.Entities.Stand;
+
+public class NavigationAppGetServices {
+
+   public HashSet getAPs (){
+       HashSet<AccessPoint> temp = new HashSet<AccessPoint>();
+       return temp;
+   }
+
+  public HashSet<Stand> getStands (){
+    HashSet<Stand> temp = new HashSet<Stand>();
+       return temp;
+   }
+}
--- app/src/main/java/technikum_wien/at/smartcity_indoornavigation/Service/NavigationAppNavigateService.java
+++ app/src/main/java/technikum_wien/at/smartcity_indoornavigation/Service/NavigationAppNavigateService.java
@@ -1,20 +1,13 @@
 package technikum_wien.at.smartcity_indoornavigation.Service;
 
 import java.util.HashSet;
-import java.util.List;
 
-import technikum_wien.at.smartcity_indoornavigation.Entities.AccessPoint;
-import technikum_wien.at.smartcity_indoornavigation.Entities.Stand;
+import technikum_wien.at.smartcity_indoornavigation.Entities.*;
 
 public class NavigationAppNavigateService {
 
-   public HashSet getAPs (){
-       HashSet<AccessPoint> temp = new HashSet<AccessPoint>();
-       return temp;
-   }
-
-  public HashSet<Stand> getStands (){
-    HashSet<Stand> temp = new HashSet<Stand>();
-       return temp;
-   }
+    public HashSet<GridPoint> navigate (GridPoint destination, HashSet<AccessPoint> accesPoints){
+        HashSet<GridPoint> temp = new HashSet<GridPoint>();
+        return temp;
+    }
 }
