b96486682ab49b864d0daf50d4f1fcf7b08a890f
eddsus <edd.sustr@gmail.com>
2019-01-12 20:04:03 +0100

    iteration of a grid point list

--- app/src/main/java/technikum_wien/at/smartcity_indoornavigation/MapView.java
+++ app/src/main/java/technikum_wien/at/smartcity_indoornavigation/MapView.java
@@ -7,9 +7,8 @@ import android.graphics.Paint;
 import android.graphics.Path;
 import android.util.AttributeSet;
 import android.view.View;
-
-import java.util.HashSet;
-import java.util.Set;
+import java.util.LinkedList;
+import java.util.List;
 
 import technikum_wien.at.smartcity_indoornavigation.Entities.GridPoint;
 
@@ -36,39 +35,33 @@ public class MapView extends View {
 
     private void init() {
         paint = new Paint();
-        paint.setColor(Color.GREEN);
+        paint.setColor(Color.BLUE);
         paint.setStrokeWidth(6);
         paint.setStyle(Paint.Style.STROKE);
-        GridPoint gp1 = new GridPoint("2", "0","0");
-        GridPoint gp2 = new GridPoint("3", "10","10");
-        GridPoint gp3 = new GridPoint("3", "20","20");
-        GridPoint gp4 = new GridPoint("3", "30","30");
-        GridPoint gp5 = new GridPoint("3", "40","40");
-        GridPoint gp6 = new GridPoint("3", "50","50");
-        GridPoint gp7 = new GridPoint("3", "60","60");
-        GridPoint gp8 = new GridPoint("3", "70","70");
-        GridPoint gp9 = new GridPoint("3", "80","80");
+        GridPoint gp1 = new GridPoint("2", "200","200");
+        GridPoint gp2 = new GridPoint("3", "700","200");
+        GridPoint gp3 = new GridPoint("4", "700","500");
+        GridPoint gp4 = new GridPoint("5", "500","500");
 
         /*GridPoint gp3 = new GridPoint("4", "100","70");
         GridPoint gp4 = new GridPoint("5", "100","300");
         GridPoint gp5 = new GridPoint("6", "700","300");
 */
-        Set<GridPoint> gps = new HashSet<GridPoint>();
+        List<GridPoint> gps = new LinkedList<>();
         gps.add(gp1);
         gps.add(gp2);
         gps.add(gp3);
         gps.add(gp4);
-        gps.add(gp5);
+
+/*        gps.add(gp5);
         gps.add(gp6);
         gps.add(gp7);
         gps.add(gp8);
-        gps.add(gp9);
+        gps.add(gp9);*/
 
         path = new Path();
 
-        for (GridPoint gp:gps) {
-            path.lineTo(Integer.parseInt(gp.getPosX()),Integer.parseInt(gp.getPosY()));
-        };
+        drawPath(gps);
 
 /*        path.lineTo(500, 500);
         path.lineTo(200, 500);
@@ -80,6 +73,27 @@ public class MapView extends View {
         path.lineTo(1181, 560);*/
 
 
+
+    }
+
+    private void drawPath(List<GridPoint> gps) {
+
+        if(gps.size() > 1) {
+            int x = Integer.parseInt(gps.get(0).getPosX());
+            int y = Integer.parseInt(gps.get(0).getPosY());
+
+            path.moveTo(x,y);
+
+            //drawCircle(x,y );
+            //path.addCircle(x,y,5, null);
+            gps.remove(0);
+
+            for (GridPoint gp : gps) {
+                path.lineTo(Integer.parseInt(gp.getPosX()), Integer.parseInt(gp.getPosY()));
+            }
+
+
+        }
     }
 
     @Override
